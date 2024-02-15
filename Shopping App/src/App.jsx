a3af6f5781ad73ed0c77eba116f71e851a15c7fb
CodeGuru15 <arijitpal6111@gmail.com>
2024-02-15 14:10:06 +0530

    setup done

---
+++ Shopping App/src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <div className=" text-2xl flex justify-center text-blue-700">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
